2b7a50c3faa777e0e7d7313562a6c7535a2079a5
JEYBAN37 <jeyban37@gmail.com>
2025-07-08 03:32:22 -0500

    jeysgegu - feature : se agrega modelos

---
+++ domain/model/src/main/java/com/sura/domain/gastoxmes/GastoxMes.java
@@ -0,0 +1,35 @@
+package com.sura.domain.gastoxmes;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Setter
+@Getter
+@Builder(toBuilder = true)
+public class GastoxMes {
+    private Date reporte ;
+    private String idEmpleado;
+    private  Date periodo;
+    private  Double total;
+    private Double iva;
+    private Double totalConIva;
+    private Boolean asume;
+    private Date cierre;
+
+    @Override
+    public String toString() {
+        return "GastoxMes{" +
+                "reporte=" + reporte +
+                ", idEmpleado='" + idEmpleado + '\'' +
+                ", periodo=" + periodo +
+                ", total=" + total +
+                ", iva=" + iva +
+                ", totalConIva=" + totalConIva +
+                ", asume=" + asume +
+                ", cierre=" + cierre +
+                '}';
+    }
+}
