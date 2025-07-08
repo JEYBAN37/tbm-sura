fa805d24dbb209e650e7e3f2e7e3cffe4c4fbfaf
JEYBAN37 <jeyban37@gmail.com>
2025-07-08 00:55:48 -0500

    jeysgegu - feature: creacion linea base

---
+++ applications/app-service/src/main/java/com/sura/tbm/MainApplication.java
@@ -0,0 +1,14 @@
+package com.sura.tbm;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "APIREST TBM", version = "1.0", description = "Documentacion APIREST TBM v1.0"))
+public class MainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+}
---
+++ applications/app-service/src/main/java/com/sura/tbm/UseCaseConfig.java
@@ -0,0 +1,9 @@
+package com.sura.tbm;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("sura.tbm")
+public class UseCaseConfig {
+}
